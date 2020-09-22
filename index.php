cdf811006c61ec2af07daff246d2f10ef6478caa
gianghaison <ghson116@gmail.com>
2020-09-22 21:56:40 +0700

    Update index.php
    test

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
 echo "Hello world lần thứ 2";
 echo "</br>";
-echo "git hub test!!!"
+echo "git hub test!!!";
 //tôi không biết
+
+$phone_numbers = [
+  "Alex" => "415-235-8573",
+  "Jessica" => "415-492-4856",
+];
+
+print_r($phone_numbers);
+echo "</br>";
+print_r(array_values($phone_numbers));
  ?>
